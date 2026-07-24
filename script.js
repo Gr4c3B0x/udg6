f18b9705ddafe34e2a2c6f02353112aa7c763b45
GiftBox <acegalaxi46@gmail.com>
2026-07-24 21:41:46 +0700

    Update script URL to use CORS proxy

--- script.js
+++ script.js
@@ -200,7 +200,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // 1. Inisialisasi Database Lokal & Konfigurasi Default
     const DEFAULT_PASS = "angkatan36nihbos";
     // Taruh URL Google Apps Script Web App Anda di bawah ini agar sinkronisasi berfungsi secara global di HP dan laptop tamu
-    const DEFAULT_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx2iMgNtNQOYGiql7ipyQk2CbE6IO_EACghWUjtKteLv7VUn8kL__mXkE2qzHaDg0aE/exec";
+    const DEFAULT_SCRIPT_URL = "https://corsproxy.io/?https://script.google.com/macros/s/AKfycbx2iMgNtNQOYGiql7ipyQk2CbE6IO_EACghWUjtKteLv7VUn8kL__mXkE2qzHaDg0aE/exec";
     const MOCK_WISHES = [
         {
             id: "mock-1",
